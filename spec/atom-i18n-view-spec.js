c7e39f6b57b51c99ce3054c360367278e6b748b1
Derek Liu <derek.liu@tw.promise.com>
2016-12-19 18:13:05 +0800

    init commit: update name, description in package.json

---
+++ spec/atom-i18n-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomI18nView from '../lib/atom-i18n-view';
+
+describe('AtomI18nView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
