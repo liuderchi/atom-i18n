581bc185c911f64a3098dac0ebece87ec790c939
liuderchi <liuderchi@gmail.com>
2017-01-19 14:20:25 +0800

    add spec: consistent number of cson keys in validation.js
    - test with atom --test
    - validate same number of values of cson

--- spec/validation.js
+++ spec/validation.js
@@ -1,5 +1,7 @@
 'use babel';
 
+// NOTE use atom --test spec/validation.js to test
+
 const path = require('path');
 const CSON = require('cson');
 
@@ -31,6 +33,60 @@ describe('validation', () => {
         for (let file of FILES) {
           let content = CSON.load(path.join(__dirname, '../def', locale, file));
           expect(content).toBeTruthy();
+        }
+      }
+    });
+
+    describe('checking each cson files of all locales should have same number of key-value pairs', () => {
+
+      JSON.flatten = function(data) {
+        var result = {};
+        function recurse (cur, prop) {
+          if (Object(cur) !== cur) {
+            result[prop] = cur;
+          } else if (Array.isArray(cur)) {
+            for(var i=0, l=cur.length; i<l; i++)
+              recurse(cur[i], prop + '[' + i + ']');
+            if (l == 0) {
+              result[prop] = [];
+            }
+          } else {
+            var isEmpty = true;
+            for (var p in cur) {
+              isEmpty = false;
+              recurse(cur[p], prop ? prop+'.'+p : p);
+            }
+            if (isEmpty && prop) {
+              result[prop] = {};
+            }
+          }
+        }
+        recurse(data, '');
+        return result;
+      };
+
+      function countKeysFromCSON(_path) {
+        return Object.keys(JSON.flatten(CSON.load(_path))).length;
+      }
+
+      let stat = {};
+      for (let file of FILES) {
+        stat[file] = [];
+        for (let locale of LOCALES) {
+          let keyCount = countKeysFromCSON(path.join(__dirname, '../def', locale, file));
+          stat[file].push(keyCount);
+        }
+        let maxCount = Math.max(...stat[file]);
+        let checkRes = Boolean((new Set(stat[file])).size === 1);
+        console.log(`${file}: ${(checkRes)?'ok':'Err: some less than ' + maxCount}`);
+      }
+
+      for (let file of FILES) {
+        for (let locale of LOCALES) {
+          it(`checks "${path.join(locale, file)}"`, () => {
+            let keyCount = countKeysFromCSON(path.join(__dirname, '../def', locale, file));
+            expect(keyCount).toBe(Math.max(...stat[file]));
+          });
         }
       }
     });
