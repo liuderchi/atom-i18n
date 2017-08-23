6195b0938492e8f3ea3605df116441890ebf21a9
liuderchi <liuderchi@gmail.com>
2017-08-23 22:39:30 +0800

    test template/settings.cson by fetching atom config-schema.js source
    - async test in mocha: method 1: use done() in mocha to handle async
    test
        - ref:
    http://staxmanade.com/2015/11/testing-asyncronous-code-with-mochajs-and-es7-async-await/
        - assert after request promise is resovled
    - add exception key list
    - npm i --save-dev axios

--- spec/validation.js
+++ spec/validation.js
@@ -17,6 +17,8 @@ const { flattenObj } = require('./util.js')
 const LOCALES = require('./locales.js')
 const CsonFiles = require('./defs.js')
 
+const ATOMVERSION = 'v1.19.0'
+
 describe('validation', () => {
 
   describe('package.json validation', () => {
@@ -36,6 +38,45 @@ describe('validation', () => {
   })
 
   describe('cson file validation', () => {
+
+    describe('checking template/settings.cson `controls.*._id` according atom config-schema.js', () => {
+
+      it('compares keys with flatten config-schema.js', done => {
+        const neverShownDesciptionInSettingsPanelItems = [
+          'core.customFileTypes',
+          'core.disabledPackages',
+          'core.themes',
+          'editor.commentEnd',
+          'editor.commentStart',
+          'editor.decreaseIndentPattern',
+          'editor.foldEndPattern',
+          'editor.increaseIndentPattern',
+          'editor.invisibles',   // NOTE shows only editor.invisibles.*
+        ]    // NOTE Manually updated exceptional list from https://github.com/atom/settings-view/blob/master/lib/settings-panel.js#L339-L350
+
+        const templateSettingsControls = CSON.load(path.join(__dirname, '../def/template', 'settings.cson'))
+          .Settings.settings.controls.map(({ _id }) => _id)
+
+        const axios = require('axios')
+        const configURL = `https://raw.githubusercontent.com/atom/atom/${ATOMVERSION}/src/config-schema.js`
+        axios.get(configURL).then(({ data }) => {
+          try {
+            const srcConfig = eval(data)
+            const flattenSrcConfigKeys = Object.keys(flattenObj(srcConfig))
+              .filter(key => key.search(/enum/g) === -1)
+              .filter(key => key.search(/description$/g) > -1)
+              .map(key => key.replace(/\.properties/g, '').replace(/\.description/g, ''))
+
+            expect(templateSettingsControls.concat(neverShownDesciptionInSettingsPanelItems))
+              .to.include.members(flattenSrcConfigKeys, 'inconsistent keys')
+            // NOTE expect all interested keys in `flattenSrcConfigKeys` appears in templateSettingsControls
+            done()
+          } catch (err) {
+            done(err)  // handle assertion fails error, to avoid test timeout
+          }
+        }, done)    // should always run done() when promise-resolved, assertion-fail-error, promise-rejected
+      })
+    })
 
     describe('checking each cson files of all locales', () => {
       const templateKeys = {}
